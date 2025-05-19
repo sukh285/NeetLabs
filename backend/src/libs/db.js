342bcbbc1524c8e9c5755a15c0e50caa7bb6dedb
Sukhmeet Singh Lamba <130579680+sukh285@users.noreply.github.com>
2025-05-19 21:25:56 +0530

    testing

--- backend/src/libs/db.js
+++ backend/src/libs/db.js
@@ -5,3 +5,6 @@ const globalForPrisma = globalThis;
 export const db = globalForPrisma.prisma || new PrismaClient();
 
 if (process.env.NODE_ENV !== "production") globalForPrisma.prisma = db;
+
+
+//comment
