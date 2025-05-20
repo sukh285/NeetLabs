a58a84dba8c7cea4ab654969990d78246824afdf
Sukhmeet Singh Lamba <130579680+sukh285@users.noreply.github.com>
2025-05-20 15:37:51 +0530

    routes, controllers, register user controller

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,10 +1,22 @@
 import express from "express";
 import dotenv from "dotenv";
+import cookieParser from "cookie-parser";
+
+import authRoutes from "./routes/auth.routes.js";
 
 dotenv.config();
 const port = process.env.PORT;
 
 const app = express();
+
+app.use(express.json());
+app.use(cookieParser());
+
+app.get("/", (req, res) => {
+  res.send("Welcome to leetlab");
+});
+
+app.use("/api/v1/auth", authRoutes);
 
 app.listen(port, () => {
   console.log("Server running on port 8080");
