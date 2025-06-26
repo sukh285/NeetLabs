78ae48272b4bde93da88ec5a5ec8b148eed40fda
Sukhmeet Singh Lamba <130579680+sukh285@users.noreply.github.com>
2025-06-26 21:47:52 +0530

    setup Vite project with Tailwind, DaisyUI, and React Router DOM, setup Routes and basic pages - (Home, Signup, Login)

---
+++ frontend/src/App.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+import {Routes, Route, Navigate} from 'react-router-dom'
+import LoginPage from './pages/LoginPage'
+import HomePage from './pages/HomePage'
+import SignUpPage from './pages/SignUpPage'
+
+const App = () => {
+  return (
+    <div className='flex flex-col items-center justify-start'>
+      <Routes>
+        <Route path='/' element={<HomePage/>}/>
+        <Route path='/login' element={<LoginPage/>}/>
+        <Route path='/signup' element={<SignUpPage/>}/>
+      </Routes>
+    </div>
+  )
+}
+
+export default App
