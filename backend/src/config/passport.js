38365b62a65479c3ed0b43c2edd8a2b3126ad03d
Sukhmeet Singh Lamba <130579680+sukh285@users.noreply.github.com>
2025-07-28 02:19:35 +0530

    google oauth error path

--- backend/src/config/passport.js
+++ backend/src/config/passport.js
@@ -10,7 +10,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: "/api/v1/auth/google/callback",
+      callbackURL: "https://neetlabs.onrender.com/api/v1/auth/google/callback",
     },
     async (accessToken, refreshToken, profile, done) => {
       try {
